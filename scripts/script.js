e9ed7b5c0b8d8006d7aed74c9379103f70e2545e
Alan Ryan <alanryan619@gmail.com>
2025-01-28 12:45:25 -0300

    feat: created a new project, added fontawesome icons and fonts.

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,56 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const imageUrls = [
-        "./assets/images/01.webp",
-        "./assets/images/02.webp",
-        "./assets/images/03.webp",
-        "./assets/images/04.webp",
-        "./assets/images/05.webp",
-        "./assets/images/06.webp",
-        "./assets/images/07.webp",
-        "./assets/images/08.webp",
-        "./assets/images/09.webp",
-        "./assets/images/10.webp",
-        "./assets/images/11.webp",
-        "./assets/images/12.webp",
-        "./assets/images/13.webp",
-        "./assets/images/14.webp",
-        "./assets/images/15.webp",
-        "./assets/images/16.webp",
-        "./assets/images/17.webp",
-        "./assets/images/18.webp",
-        "./assets/images/19.webp",
-        "./assets/images/20.webp",
-        "./assets/images/21.webp",
-        "./assets/images/22.webp",
-        "./assets/images/23.webp",
-        "./assets/images/24.webp",
-        "./assets/images/25.webp",
-        "./assets/images/26.webp",
-        "./assets/images/27.webp",
-    ];
+// document.addEventListener("DOMContentLoaded", () => {
+//     const imageUrls = [
+//         "./assets/images/01.webp",
+//         "./assets/images/02.webp",
+//         "./assets/images/03.webp",
+//         "./assets/images/04.webp",
+//         "./assets/images/05.webp",
+//         "./assets/images/06.webp",
+//         "./assets/images/07.webp",
+//         "./assets/images/08.webp",
+//         "./assets/images/09.webp",
+//         "./assets/images/10.webp",
+//         "./assets/images/11.webp",
+//         "./assets/images/12.webp",
+//         "./assets/images/13.webp",
+//         "./assets/images/14.webp",
+//         "./assets/images/15.webp",
+//         "./assets/images/16.webp",
+//         "./assets/images/17.webp",
+//         "./assets/images/18.webp",
+//         "./assets/images/19.webp",
+//         "./assets/images/20.webp",
+//         "./assets/images/21.webp",
+//         "./assets/images/22.webp",
+//         "./assets/images/23.webp",
+//         "./assets/images/24.webp",
+//         "./assets/images/25.webp",
+//         "./assets/images/26.webp",
+//         "./assets/images/27.webp",
+//     ];
 
-    const images = document.querySelectorAll(".dynamic-image");
+//     const images = document.querySelectorAll(".dynamic-image");
 
-    let lastImage = null;
+//     let lastImage = null;
 
-    images.forEach((img) => {
-        let randomImage;
-        do {
-            randomImage = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-        } while (randomImage === lastImage);
-        lastImage = randomImage;
+//     images.forEach((img) => {
+//         let randomImage;
+//         do {
+//             randomImage = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+//         } while (randomImage === lastImage);
+//         lastImage = randomImage;
 
-        img.loading = "lazy";
-        img.src = randomImage;
-        img.alt = "Imagem padrão";
+//         img.loading = "lazy";
+//         img.src = randomImage;
+//         img.alt = "Imagem padrão";
 
-        img.addEventListener("error", () => {
-            img.loading = "lazy";
-            img.src = "./assets/images/default.webp";
-            img.alt = "Imagem padrão";
-        });
-    });
-});
+//         img.addEventListener("error", () => {
+//             img.loading = "lazy";
+//             img.src = "./assets/images/default.webp";
+//             img.alt = "Imagem padrão";
+//         });
+//     });
+// });
 
 window.addEventListener('scroll', function () {
     const scrollTopButton = document.getElementById('scrollTopButton');
